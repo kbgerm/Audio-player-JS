00f3ace842b33df3f5a088491a26d23add9a2825
kb_germ <k.b.germ1806@gmail.com>
2024-09-21 20:47:10 +0300

    feat: player

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
+const songs = [
+  {
+    name: 'Madonna - Like a prayer',
+    src: './assets/Madonna – Like a Prayer.mp3',
+    img: './assets/Madonna_cover.png'
+  },
+  {
+    name: 'alt-J - In Cold Blood',
+    src: './assets/alt-j-in-cold-blood.mp3',
+    img: './assets/In_Cold_Blood_alt-J.jpg'
+  },
+  {
+    name: 'Foster the People - Pumped Up Kicks',
+    src: './assets/foster-the-people-pumped-up-kicks.mp3',
+    img: './assets/PumpedUpKicks.jpg'
+  },
+  {
+    name: 'Валерий Меладзе - Говновоз',
+    src: './assets/Валерий Меладзе - Говновоз.mp3',
+    img: './assets/ValeryGovno.png'
+  }
+];
+
+
 const playBtn = document.querySelector('.playButton');
 const pauseBtn = document.querySelector('.pauseButton');
-let isPlay = false;
+const prev = document.querySelector('.prevButton');
+const next = document.querySelector('.nextButton');
 const progressBar = document.querySelector('.progress');
+const bar = document.querySelector('.bar');
 const duration = document.querySelector('.wholeTime');
-const current = document.querySelector('.currenTime');
+const current = document.querySelector('.currentTime');
+const picture = document.querySelector('.NPImg');
+const text = document.querySelector('.current');
+let cover = document.createElement('img');
+let songText = document.createElement('p');
 
-const audio = new Audio('./assets/Madonna – Like a Prayer.mp3');
+var playNum = 0;
+let isPlay = false;
 
+let song = songs[playNum];
+const audio = new Audio();
 console.dir(audio);
 
+function songImage(playNum) {
+  song = songs[playNum];
+  cover.className = 'img';
+  cover.src = song.img;
+  picture.appendChild(cover);
+}
+function songTitle(playNum) {
+  song = songs[playNum];
+  songText.className = 'currentText';
+  songText.textContent = song.name;
+  text.appendChild(songText);
+}
+
 function playAudio() {
-  audio.currentTime = 0;
+  audio.src = song.src;
   if (!isPlay) {
-    // audio.src = './assets/Madonna – Like a Prayer.mp3';
     pauseBtn.classList.remove('hidden');
     pauseBtn.classList.add('visible');
     playBtn.classList.add('hidden');
@@ -32,7 +77,10 @@ audio.addEventListener('timeupdate', progress);
 function progress() {
   const progressBar = document.querySelector(".progress");
   progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
-  current.innerHTML
+  let dur = audio.duration;
+  let cur = audio.currentTime;
+  duration.innerHTML = duration.innerHTML.replace('0:00', getNormalTime(dur));
+  current.innerHTML = getNormalTime(cur);
 };
 
 playBtn.addEventListener('click', playAudio);
@@ -42,5 +90,56 @@ document.addEventListener('DOMContentLoaded', start);
 
 function start() {
   pauseBtn.classList.add('hidden');
-
+  songImage(playNum);
+  songTitle(playNum);
 }
+
+function getNormalTime(s) {
+  let m = Math.trunc(s / 60) + '';
+  s = Math.trunc(s % 60) + '';
+
+  return m.padStart(2, 0) + ':' + s.padStart(2, 0);
+}
+
+bar.addEventListener('click', e => {
+  const timelineWidth = window.getComputedStyle(bar).width;
+  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+  audio.currentTime = timeToSeek;
+});
+
+function playNext() {
+  nextSong();
+  if (playNum > songs.length - 1) {
+    playNum = 0;
+  }
+  cover.remove();
+  songText.remove();
+  songImage(playNum);
+  songTitle(playNum);
+}
+
+function playPrev() {
+  prevSong();
+  if (playNum < 0) {
+    playNum = songs.length;
+  } else {
+    cover.remove();
+    songText.remove();
+    songImage(playNum);
+    songTitle(playNum);
+  }
+}
+
+function prevSong() {
+  playNum = playNum - 1;
+  return prevSong;
+}
+
+function nextSong() {
+  playNum = playNum + 1;
+  return playNum;
+}
+
+next.addEventListener('click', playNext);
+prev.addEventListener('click', playPrev);
+
